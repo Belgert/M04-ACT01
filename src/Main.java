7ba88f8460098c6b4857efbbd2c5c8a853595e00
Becky <rebecca.e.reyes@outlook.com>
2023-03-10 09:54:49 -0500

    Added print statement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //Added a different print statement since I already hah Hello World
+        System.out.println("Committed a new change, whoa!");
     }
 }
